7c0641625c2b4538e8832e5adb2d14883228359a
smartprintorhub <nikitadigiconnect19@gmail.com>
2025-07-16 13:09:13 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Dell product support'
 
 
 # The full version, including alpha/beta/rc tags
@@ -25,7 +25,7 @@ project = 'HP Smart'
 
 
 html_meta = {
-    "msvalidate.01": "108BF3BCC1EC90CA1EBEFF8001FAEFEA"
+    "msvalidate.01": "81B028225CE50EE5EDE47254C5F71B08"
    
 }
 
